5628d7f03ad2adfc9743abfdb95b8f954559d53f
dagreen3 <dongree@cisco.com>
2016-03-29 16:33:30 -0600

    changed to italics

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>This is a house of learned doctors.</h1>")
+	io.WriteString(w, "<em>This is a house of learned doctors.</em>")
 	io.WriteString(w, "<a href=\"https://www.youtube.com/watch?v=hh1oaumUoyc\">https://www.youtube.com/watch?v=hh1oaumUoyc</a>")
 	fmt.Fprintf(w, "<h2>Hi there, I love %s!</h2>", r.URL.Path[1:])
 }
